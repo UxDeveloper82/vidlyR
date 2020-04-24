6751aa01031711b39bbba1d7df4f8e682458595a
UxDeveloper82 <electronicsmf@aol.com>
2020-04-23 22:16:29 -0400

    Display the list of customers and movies

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -10,6 +10,23 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+
+        // GET: Movies
+        public ActionResult Index()
+        {
+            var movies = GetMovies();
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+           {
+              new Movie { Id =1, Name ="Shrek"},
+              new Movie { Id =2, Name ="Wall-e"}
+           };
+        }
+
         //GET: Movies/Random
         public ActionResult Random() 
         {
@@ -29,10 +46,5 @@ namespace Vidly.Controllers
             return View(viewModel);
         }
 
-        // GET: Movies
-        public ActionResult Index()
-        {
-            return View();
-        }
     }
 }
