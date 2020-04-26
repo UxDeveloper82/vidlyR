d4bce2db8fc7947cd0411d280e47d59737c050f4
UxDeveloper82 <electronicsmf@aol.com>
2020-04-26 14:53:21 -0400

    Add validation to customer form.

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -4,6 +4,7 @@ using System.Data.Entity;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Web;
+using System.Web.ModelBinding;
 using System.Web.Mvc;
 using Vidly.Models;
 using Vidly.ViewModels;
@@ -54,8 +55,19 @@ namespace Vidly.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Save(Customer customer)
         {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new CustomerFormViewModel
+                {
+                    Customer = customer,
+                    MembershipTypes = _context.MembershipTypes.ToList()
+                };
+                return View("CustomerForm", viewModel);
+            }
+
             if (customer.Id == 0)
                 _context.Customers.Add(customer);
             else
--- Vidly/Models/MembershipType.cs
+++ Vidly/Models/MembershipType.cs
@@ -11,12 +11,12 @@ namespace Vidly.Models
         public byte Id { get; set; }
         [Required]
         public string Name { get; set; }
-
         public short SignUpFee { get; set; }
-
         public byte DurationInMonths { get; set; }
-
         public byte DiscountRate { get; set; }
+
+        public static readonly byte Unknown = 0;
+        public static readonly byte PayAsYouGo = 1;
 
     }
 }
